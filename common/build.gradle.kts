44be2c93e4b54acb66e3a05e052233d0edc7ebd8
Nils Jäkel <nils.jaekel@redtronics.dev>
2025-03-01 15:39:10 +0100

    feat: apply native configuration

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -10,14 +10,12 @@
  */
 
 import dev.redtronics.buildsrc.Project
+import dev.redtronics.buildsrc.cinterop.applyNativeConfiguration
 
 plugins {
     `mokt-core`
     `mokt-build-constants`
-//    `mokt-publishing`
-//    `mokt-multiplatform`
-//    `mokt-cinterop-generation`
-//    `mokt-build-constants`
+    `mokt-cinterop-generation`
 }
 
 repositories {
@@ -27,151 +25,110 @@ repositories {
 
 group = Project.GROUP
 
-mokt {
-    buildConstants {
-        properties = mapOf(
-            "API_VERSION" to "1.0.0",
-        )
+kotlin {
+    js(IR) {
+        generateTypeScriptDefinitions()
+        nodejs()
+        useEsModules()
+        binaries.library()
     }
 
-    docs {
+    linuxX64 { applyNativeConfiguration() }
+    mingwX64 { applyNativeConfiguration() }
+    macosX64 { applyNativeConfiguration() }
+    macosArm64 { applyNativeConfiguration() }
+    iosArm64 { applyNativeConfiguration() }
+    iosX64 { applyNativeConfiguration() }
+    iosSimulatorArm64 { applyNativeConfiguration() }
+    watchosArm32 { applyNativeConfiguration() }
+    watchosArm64 { applyNativeConfiguration() }
+    watchosX64 { applyNativeConfiguration() }
+    watchosSimulatorArm64 { applyNativeConfiguration() }
+    tvosArm64 { applyNativeConfiguration() }
+    tvosX64 { applyNativeConfiguration() }
+    tvosSimulatorArm64 { applyNativeConfiguration() }
 
+    sourceSets {
+        commonMain {
+            dependencies {
+                // Serialization
+                api(libs.kotlinx.serialization.json)
+
+                // Ktor Client
+                api(libs.ktor.client.core)
+                api(libs.ktor.client.logging)
+                api(libs.ktor.client.content.negotiation)
+
+                // Ktor Common
+                api(libs.ktor.serialization.json)
+
+                // Datetime
+                api(libs.kotlinx.datetime)
+            }
+        }
+
+        jvmMain {
+            dependencies {
+                // Ktor Client
+                api(libs.ktor.client.cio)
+
+                // Logging
+                api(libs.logback)
+            }
+        }
+
+        jsMain {
+            dependencies {
+                api(libs.ktor.client.js)
+                implementation(npm("open", "10.1.0"))
+            }
+        }
+
+        linuxMain {
+            dependencies {
+                api(libs.ktor.client.cio)
+            }
+        }
+
+        mingwMain {
+            dependencies {
+                api(libs.ktor.client.winhttp)
+            }
+        }
+
+        iosMain {
+            dependencies {
+                api(libs.ktor.client.darwin)
+            }
+        }
+
+        macosMain {
+            dependencies {
+                api(libs.ktor.client.darwin)
+            }
+        }
+
+        tvosMain {
+            dependencies {
+                api(libs.ktor.client.darwin)
+            }
+        }
+
+        watchosMain {
+            dependencies {
+                // Ktor Client
+                api(libs.ktor.client.darwin)
+            }
+        }
+
+        all {
+            languageSettings {
+                optIn("kotlinx.cinterop.UnsafeNumber")
+                optIn("kotlinx.cinterop.ExperimentalForeignApi")
+                optIn("kotlin.experimental.ExperimentalNativeApi")
+                optIn("kotlin.native.runtime.NativeRuntimeApi")
+                optIn("kotlin.ExperimentalStdlibApi")
+            }
+        }
     }
 }
-
-//kotlin {
-//    js(IR) {
-//        generateTypeScriptDefinitions()
-//        nodejs()
-//        useEsModules()
-//        binaries.library()
-//    }
-
-//    val nativeDefFilePath = Path("../native-cinterop/cinterop.def")
-//    linuxX64 {
-//        applyCInteropGeneration(nativeDefFilePath)
-//    }
-//
-//    mingwX64 {
-//        applyCInteropGeneration(nativeDefFilePath)
-//    }
-//
-//    macosX64 {
-//        applyCInteropGeneration(nativeDefFilePath)
-//    }
-//    macosArm64 {
-//        applyCInteropGeneration(nativeDefFilePath)
-//    }
-//
-//    iosArm64 {
-//        applyCInteropGeneration(nativeDefFilePath)
-//    }
-//    iosX64 {
-//        applyCInteropGeneration(nativeDefFilePath)
-//    }
-//    iosSimulatorArm64 {
-//        applyCInteropGeneration(nativeDefFilePath)
-//    }
-//
-//    watchosArm32 {
-//        applyCInteropGeneration(nativeDefFilePath)
-//    }
-//    watchosArm64 {
-//        applyCInteropGeneration(nativeDefFilePath)
-//    }
-//    watchosX64 {
-//        applyCInteropGeneration(nativeDefFilePath)
-//    }
-//    watchosSimulatorArm64 {
-//        applyCInteropGeneration(nativeDefFilePath)
-//    }
-//
-//    tvosArm64 {
-//        applyCInteropGeneration(nativeDefFilePath)
-//    }
-//    tvosX64 {
-//        applyCInteropGeneration(nativeDefFilePath)
-//    }
-//    tvosSimulatorArm64 {
-//        applyCInteropGeneration(nativeDefFilePath)
-//    }
-
-//    sourceSets {
-//        commonMain {
-//            dependencies {
-//                api(libs.ktor.serialization.json)
-//                api(libs.ktor.client.core)
-//                api(libs.ktor.client.logging)
-//                api(libs.ktor.client.content.negotiation)
-//
-//                api(libs.kotlin.reflect)
-//                api(libs.kotlinx.serialization.json)
-//                api(libs.kotlinx.datetime)
-//            }
-//        }
-//    }
-
-//    commonTest {
-//        dependencies {
-//            implementation(libs.kotest.assertions.core)
-//            implementation(libs.kotest.framework.engine)
-//            implementation(libs.kotest.property)
-//        }
-//    }
-
-//    jvmMain {
-//        dependencies {
-//            api(libs.ktor.client.cio)
-//            api(libs.logback.classic)
-//        }
-//    }
-//
-//    jvmTest {
-//        dependencies {
-//            implementation(libs.kotest.runner.junit5)
-//        }
-//    }
-//
-//    jsMain {
-//        dependencies {
-//            api(libs.ktor.client.js)
-//            implementation(npm("open", "10.1.0"))
-//        }
-//    }
-//
-//    linuxMain {
-//        dependencies {
-//            api(libs.ktor.client.cio)
-//        }
-//    }
-//
-//    mingwMain {
-//        dependencies {
-//            api(libs.ktor.client.winhttp)
-//        }
-//    }
-//
-//    iosMain {
-//        dependencies {
-//            api(libs.ktor.client.darwin)
-//        }
-//    }
-//
-//    macosMain {
-//        dependencies {
-//            api(libs.ktor.client.darwin)
-//        }
-//    }
-//
-//    tvosMain {
-//        dependencies {
-//            api(libs.ktor.client.darwin)
-//        }
-//    }
-//
-//    watchosMain {
-//        dependencies {
-//            api(libs.ktor.client.darwin)
-//        }
-//    }
-//}
