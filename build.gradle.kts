d144df90a4038a29b2c32ae395273c665e57fad7
Nils Jäkel <nils.jaekel@redtronics.dev>
2025-03-01 15:39:21 +0100

    refactor: remove version

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,6 @@ plugins {
     org.jetbrains.dokka
 }
 
-version = System.getenv(/* name = */ "CI_COMMIT_TAG") ?: System.getenv(/* name = */ "CI_COMMIT_SHORT_SHA")?.let { "$it-dev" } ?: "0.0.0-DEV"
-
 repositories {
     mavenCentral()
 }
