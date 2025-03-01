63661e5583092ceea4d19107c3ba9f937df6ac99
Nils Jäkel <nils.jaekel@redtronics.dev>
2025-03-01 12:49:57 +0100

    feat: set default values for the build constants

--- buildSrc/src/main/kotlin/dev/redtronics/buildsrc/constants/BuildConstantsConfiguration.kt
+++ buildSrc/src/main/kotlin/dev/redtronics/buildsrc/constants/BuildConstantsConfiguration.kt
@@ -1,16 +1,29 @@
-package dev.redtronics.buildsrc.constants/*
- * MIT License
- * Copyright 2024 Nils Jäkel & David Ernst
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the “Software”),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software.
- */
+package dev.redtronics.buildsrc.constants
 
+import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.MapProperty
+import javax.inject.Inject
 
-interface BuildConstantsConfiguration {
-    val properties: MapProperty<String, String>
+/**
+ * Contains the configuration for the build constants.
+ *
+ * @since 0.0.1
+ * @author Nils Jäkel
+ */
+abstract class BuildConstantsConfiguration @Inject constructor(objects: ObjectFactory) {
+    /**
+     * Contains the properties to add to the build constants.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
+    val properties: MapProperty<String, String> = objects.mapProperty(String::class.java, String::class.java).apply { set(emptyMap()) }
+
+    /**
+     * Makes the build constants as internal source.
+     *
+     * @since 0.0.1
+     * @author Nils Jäkel
+     * */
+    val onlyInternal = objects.property(Boolean::class.java).apply { set(true) }
 }
